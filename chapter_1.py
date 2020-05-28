1c0a82df9c04ddc596c44c6805f4ec4f5218f0bc
= <zainr7989@gmail.com>
2020-05-28 16:23:30 -0400

    Utilize image dialation and erosion.

--- chapter_1.py
+++ chapter_1.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 
 if __name__ == '__main__':
--- chapter_2.py
+++ chapter_2.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 '''Basic Function Everyone Should Know'''
 
@@ -10,10 +11,18 @@ if __name__ == '__main__':
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # blur the image - asks for a value for the param kernelSize
     img_blur = cv2.GaussianBlur(img_gray, (7, 7), 0)
-    # use of an edge detector - param valyes are for the threshold
+    # use of an edge detector - param values are for the threshold
     img_canny = cv2.Canny(img, 100, 100)
+    # image dilation - increases the thickness of edges
+    # kernel matrix defined using Numpy
+    kernel = np.ones((5, 5), np.uint8)
+    img_dialation = cv2.dilate(img_canny, kernel, iterations=1)
+    # image errosion - making the image "thinner"
+    img_eroded = cv2.erode(img_dialation, kernel, iterations=1)
     # show the images
     # cv2.imshow("Gray Image", img_gray)
     # cv2.imshow("Blurred Image", img_gray)
-    cv2.imshow("Image w/ Only Edges", img_canny)
+    # cv2.imshow("Image w/ Only Edges", img_canny)
+    cv2.imshow("Dialated Image", img_dialation)
+    cv2.imshow("Eroded Image", img_eroded)
     cv2.waitKey(10000)
