b9bfb24dc7efd8ecde8769bb56a4e42bbf81d5c4
= <zainr7989@gmail.com>
2020-05-28 16:11:18 -0400

    Use an edge detector.

--- chapter_2.py
+++ chapter_2.py
@@ -10,7 +10,10 @@ if __name__ == '__main__':
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # blur the image - asks for a value for the param kernelSize
     img_blur = cv2.GaussianBlur(img_gray, (7, 7), 0)
+    # use of an edge detector - param valyes are for the threshold
+    img_canny = cv2.Canny(img, 100, 100)
     # show the images
-    cv2.imshow("Gray Image", img_gray)
-    cv2.imshow("Blurred Image", img_gray)
-    cv2.waitKey(7000)
+    # cv2.imshow("Gray Image", img_gray)
+    # cv2.imshow("Blurred Image", img_gray)
+    cv2.imshow("Image w/ Only Edges", img_canny)
+    cv2.waitKey(10000)
