187327de7fee136f4dff231a049175f37d5ffcbf
= <zainr7989@gmail.com>
2020-05-28 14:31:42 -0400

    Switch to an imagw with more convenient dimensions.

--- chapter_1.py
+++ chapter_1.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     print("Package imported")
 
     # reading in an image file
-    img = cv2.imread('jerusalem.jpg')
-
+    img = cv2.imread('static/mugshot.png')
     # showing an image
     cv2.imshow("Output", img)
+    # delaying the amount of time the image stays
+    cv2.waitKey(7000)
