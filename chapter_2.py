ab0718b7175efdfcb76a2925044587d7813df3e6
= <zainr7989@gmail.com>
2020-05-28 16:07:44 -0400

    Show gray-scaled and blurry images.

--- chapter_2.py
+++ chapter_2.py
@@ -8,6 +8,9 @@ if __name__ == '__main__':
     img = cv2.imread('static/mugshot.png')
     # convert to grayscale
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # show the image
+    # blur the image - asks for a value for the param kernelSize
+    img_blur = cv2.GaussianBlur(img_gray, (7, 7), 0)
+    # show the images
     cv2.imshow("Gray Image", img_gray)
+    cv2.imshow("Blurred Image", img_gray)
     cv2.waitKey(7000)
