7658334ebc6ab4a06b00b01eabe569438a851653
= <zainr7989@gmail.com>
2020-05-28 20:06:45 -0400

    Demo joining images with different channels, horizontally and vertically.

--- chapter_6.py
+++ chapter_6.py
@@ -17,13 +17,17 @@ def stack_images(scale, img_array):
         1. Images can be resized
         2. Images do not need to have the same number of "channels"
             (either both being RGB, or both being black and white)
+
+    Thanks to these improvements, the function is able to construct joined
+    images in both the horizontal and vertical directions.
+
     """
     rows = len(img_array)
     cols = len(img_array[0])
     rows_available = isinstance(img_array[0], list)
     width = img_array[0][0].shape[1]
     height = img_array[0][0].shape[0]
-    if rows_available:
+    if rows_available is True:
         for x in range(rows):
             for y in range(cols):
                 if img_array[x][y].shape[:2] == img_array[0][0].shape[:2]:
@@ -38,8 +42,8 @@ def stack_images(scale, img_array):
                     img_array[x][y] = cv2.cvtColor(img_array[x][y],
                                                    cv2.COLOR_GRAY2BGR)
         img_blank = np.zeros((height, width, 3), np.uint8)
-        hor = [img_blank]*rows
-        hor_con = [img_blank]*rows
+        hor = [img_blank] * rows
+        hor_con = [img_blank] * rows
         for x in range(0, rows):
             hor[x] = np.hstack(img_array[x])
         ver = np.vstack(hor)
@@ -63,12 +67,18 @@ if __name__ == '__main__':
     img = cv2.imread("static/mugshot.png")
 
     # create a horizontal stack
-    img_hor = np.hstack((img, img))
+    # img_hor = np.hstack((img, img))
     # create a vertical stack
-    img_ver = np.vstack((img, img))
+    # img_ver = np.vstack((img, img))
+
+    # joining using the custom function above
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # converts RBG to B&W
+    img_stack = stack_images(0.5, ([img, img_gray, img],  # hor and ver stack
+                                   [img, img, img]))  # must be square matrix
 
     # Show the images
-    cv2.imshow("Horizontal", img_hor)
-    cv2.imshow("Vertical", img_ver)
+    # cv2.imshow("Horizontal", img_hor)
+    # cv2.imshow("Vertical", img_ver)
+    cv2.imshow("Image Stack", img_stack)
 
     cv2.waitKey(100000)
